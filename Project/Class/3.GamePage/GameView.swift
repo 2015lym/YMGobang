d0627400360ea91167cd5e45743ce58d1e4ace28
Lym <devilx2015@gmail.com>
2017-03-13 14:02:45 +0800

    2017.3.13

--- Project/Class/3.GamePage/GameView.swift
+++ Project/Class/3.GamePage/GameView.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class GameView: UIView {
 
+    //棋盘格数，可修改此参数定制棋盘大小
+    let chessPiecesNumber = 10
+    
     override func layoutSubviews() {
         super.layoutSubviews()
         backgroundColor = UIColor.green
@@ -19,25 +22,25 @@ class GameView: UIView {
     override func draw(_ rect: CGRect) {
         super.draw(rect)
 
-        let gameSize = Double(frame.size.width - 20)
+        let gameViewSize = Double(frame.size.width - 20)
         
         let context = UIGraphicsGetCurrentContext()
         
         context?.setLineWidth(1.0)
         
         
-        for i in 0..<11 {
+        for i in 0..<chessPiecesNumber {
             
-            context?.move(to: CGPoint(x: 10, y: 10 + gameSize / 10 * Double(i)))
-            context?.addLine(to: CGPoint(x: gameSize + 10, y: 10 + gameSize / 10 * Double(i)))
+            context?.move(to: CGPoint(x: 10, y: 10 + gameViewSize / Double(chessPiecesNumber - 1) * Double(i)))
+            context?.addLine(to: CGPoint(x: gameViewSize + 10, y: 10 + gameViewSize / Double(chessPiecesNumber - 1) * Double(i)))
             
             context?.strokePath()
         }
 
-        for i in 0..<11 {
+        for i in 0..<chessPiecesNumber {
             
-            context?.move(to: CGPoint(x: 10 + gameSize / 10 * Double(i), y: 10))
-            context?.addLine(to: CGPoint(x: 10 + gameSize / 10 * Double(i), y: gameSize + 10))
+            context?.move(to: CGPoint(x: 10 + gameViewSize / Double(chessPiecesNumber - 1) * Double(i), y: 10))
+            context?.addLine(to: CGPoint(x: 10 + gameViewSize / Double(chessPiecesNumber - 1) * Double(i), y: gameViewSize + 10))
             
             context?.strokePath()
         }
